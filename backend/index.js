1cf2c8710613a69cb0cf4e540a0d21c90e8fff10
unknown <Rohit Sah>
2023-09-18 15:41:16 +0530

    Second Commit

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,26 @@
-const express = require('express')
-const notes = require('./data/notes')
-const dotenv = require('dotenv').config()
+const express = require('express');
+const notes = require('./data/notes');
+const dotenv = require('dotenv').config();
+const cors = require('cors'); // Import the cors middleware
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res)=>{
-    res.send('Hello World')
-})
+// Use the cors middleware to enable CORS for all routes
+app.use(cors());
 
-app.get('/notes', (req, res)=>{
-    res.send(notes)
-})
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
-app.get('/notes/:id', (req, res)=>{
-    const note = notes[req.params.id]
-    res.send(note)
-})
+app.get('/notes', (req, res) => {
+  res.send(notes);
+});
 
-app.listen(port, console.log(`server started on ${port} `))
+app.get('/notes/:id', (req, res) => {
+  const note = notes[req.params.id];
+  res.send(note);
+});
+
+app.listen(port, console.log(`server started on ${port}`));
